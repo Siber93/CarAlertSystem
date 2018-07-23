33c7c539c8683b4cdd6f9ed92ff17cd8bbab16ab
Simone Bertolini <siber93@hotmail.it>
2018-07-23 21:13:26 +0200

    Aggiunto progetto RSSI

---
+++ RSSItest/app/src/main/java/it/siber93/rssitest/MainActivity.java
@@ -0,0 +1,13 @@
+package it.siber93.rssitest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
