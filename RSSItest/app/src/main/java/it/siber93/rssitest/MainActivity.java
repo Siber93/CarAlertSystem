df1e8bd0d2e1bed56fa1fcac04cbfc158ec7af7c
Simone Bertolini <siber93@hotmail.it>
2018-07-26 08:54:45 +0200

    [RSSItest] Generato grafico con numeri random
    TODO_1: Inserire numeri relativi ad un beacon identificato
    TODO_2: Riorganizzare plot dei dati ed eventualmente inserire esportazione su csv
    TODO_3: Inserire distanza reale
    TODO_4: Serie1 = Beacon, Serie2 = dati con segnale Wifi
    
    TODO*: Implementare app per inviare beacon

--- RSSItest/app/src/main/java/it/siber93/rssitest/MainActivity.java
+++ RSSItest/app/src/main/java/it/siber93/rssitest/MainActivity.java
@@ -1,13 +1,290 @@
 package it.siber93.rssitest;
 
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.androidplot.Plot;
+import com.androidplot.util.PixelUtils;
+import com.androidplot.xy.BoundaryMode;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.StepMode;
+import com.androidplot.xy.XYGraphWidget;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer {
+
+    // Beacon discovery variables
+    protected static final String TAG = "RangingActivity";
+    private BeaconManager beaconManager;
+
+    // Redraws a plot whenever an update is received:
+    private class MyPlotUpdater implements Observer {
+        Plot plot;
+
+        public MyPlotUpdater(Plot plot) {
+            this.plot = plot;
+        }
+
+        @Override
+        public void update(Observable o, Object arg) {
+            plot.redraw();
+        }
+    }
+
+    // Plot graphic object
+    private XYPlot dynamicPlot;
+
+    // Plot updater, observer of the data struct
+    private MyPlotUpdater plotUpdater;
+
+    // Data is the Runnuble that update plot series and that must be observed
+    SampleDynamicXYDatasource data;
+
+    // Thread that generates new data
+    private Thread myThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Beacon manager creation
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
+        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
+        // beaconManager.getBeaconParsers().add(new BeaconParser().
+        //        setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        beaconManager.bind(this);
+
+        // Plot initialization
+        // get handles to our View defined in layout.xml:
+        dynamicPlot = (XYPlot) findViewById(R.id.plot);
+
+        // Initialize the data-observer/plot-updater
+        plotUpdater = new MyPlotUpdater(dynamicPlot);
+
+        // only display whole numbers in domain labels
+        dynamicPlot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).
+                setFormat(new DecimalFormat("0"));
+
+        // Initialize data struct
+        data = new SampleDynamicXYDatasource();
+        SampleDynamicSeries sine1Series = new SampleDynamicSeries(data, 0, "Sine 1");
+        SampleDynamicSeries sine2Series = new SampleDynamicSeries(data, 1, "Sine 2");
+
+        // Creating the formatter for graph line 1
+        LineAndPointFormatter formatter1 = new LineAndPointFormatter(
+                Color.rgb(0, 200, 0), null, null, null);
+        formatter1.getLinePaint().setStrokeJoin(Paint.Join.ROUND);
+        formatter1.getLinePaint().setStrokeWidth(10);
+
+        // Add serie 1 to the plot
+        dynamicPlot.addSeries(sine1Series,
+                formatter1);
+
+        // Creating the formatter for graph line 2
+        LineAndPointFormatter formatter2 =
+                new LineAndPointFormatter(Color.rgb(0, 0, 200), null, null, null);
+        formatter2.getLinePaint().setStrokeWidth(10);
+        formatter2.getLinePaint().setStrokeJoin(Paint.Join.ROUND);
+
+        // Add serie 2 to the plot
+        dynamicPlot.addSeries(sine2Series, formatter2);
+
+        // Hook up the plotUpdater to the data model:
+        data.addObserver(plotUpdater);
+
+        // thin out domain tick labels so they dont overlap each other:
+        dynamicPlot.setDomainStepMode(StepMode.INCREMENT_BY_VAL);
+        dynamicPlot.setDomainStepValue(5);
+
+        dynamicPlot.setRangeStepMode(StepMode.INCREMENT_BY_VAL);
+        dynamicPlot.setRangeStepValue(10);
+
+        dynamicPlot.getGraph().getLineLabelStyle(
+                XYGraphWidget.Edge.LEFT).setFormat(new DecimalFormat("###.#"));
+
+        // Freeze the range boundaries:
+        dynamicPlot.setRangeBoundaries(-100, 100, BoundaryMode.FIXED);
+
+        // create a dash effect for domain and range grid lines:
+        /*DashPathEffect dashFx = new DashPathEffect(
+                new float[] {PixelUtils.dpToPix(3), PixelUtils.dpToPix(3)}, 0);
+        dynamicPlot.getGraph().getDomainGridLinePaint().setPathEffect(dashFx);
+        dynamicPlot.getGraph().getRangeGridLinePaint().setPathEffect(dashFx);*/
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+                }
+            }
+        });
+
+        try
+        {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        }
+        catch (RemoteException e)
+        {
+
+        }
+    }
+    @Override
+    public void onResume() {
+        // kick off the data generating thread:
+        myThread = new Thread(data);
+        myThread.start();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        data.stopThread();
+        super.onPause();
+    }
+
+    // Class that generate the dynamic data
+    class SampleDynamicXYDatasource implements Runnable {
+
+        // encapsulates management of the observers watching this datasource for update events:
+        class MyObservable extends Observable {
+            @Override
+            public void notifyObservers() {
+                setChanged();
+                super.notifyObservers();
+            }
+        }
+
+
+        private ArrayList<Number> numbersY =                        // List of Y values
+                new ArrayList<Number>();
+        private MyObservable notifier;
+        private boolean keepRunning = false;                        // Thread is running or not
+        private int time = 0;                                       // Seconds from start
+
+
+
+        {
+            notifier = new MyObservable();
+        }
+
+        // Stop thread running cycle
+        public void stopThread() {
+            keepRunning = false;
+        }
+
+        // Make while cycle start
+        public void run() {
+            try {
+                keepRunning = true;
+                boolean isRising = true;
+                while (keepRunning) {
+
+                    Thread.sleep(1000); // decrease or remove to speed up the refresh rate.
+                    numbersY.add(new Random().nextInt(200)-100);
+                    time++;
+
+                    notifier.notifyObservers();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public int getItemCount(int series) {
+            return time;
+        }
+
+        public Number getX(int series, int index) {
+            if (index >= time) {
+                throw new IllegalArgumentException();
+            }
+            return index;
+        }
+
+        public Number getY(int series, int index) {
+            if (index >= time) {
+                throw new IllegalArgumentException();
+            }
+
+            switch(series){
+                case 0:
+                    return numbersY.get(index);
+                case 1:
+                    return -1*(int)numbersY.get(index);
+                default: return 0;
+            }
+        }
+
+        public void addObserver(Observer observer) {
+            notifier.addObserver(observer);
+        }
+
+        public void removeObserver(Observer observer) {
+            notifier.deleteObserver(observer);
+        }
+
+    }
+
+    class SampleDynamicSeries implements XYSeries {
+        private SampleDynamicXYDatasource datasource;
+        private int seriesIndex;
+        private String title;
+
+        public SampleDynamicSeries(SampleDynamicXYDatasource datasource, int seriesIndex, String title) {
+            this.datasource = datasource;
+            this.seriesIndex = seriesIndex;
+            this.title = title;
+        }
+
+        @Override
+        public String getTitle() {
+            return title;
+        }
+
+        @Override
+        public int size() {
+            return datasource.getItemCount(seriesIndex);
+        }
+
+        @Override
+        public Number getX(int index) {
+            return datasource.getX(seriesIndex, index);
+        }
+
+        @Override
+        public Number getY(int index) {
+            return datasource.getY(seriesIndex, index);
+        }
     }
 }
